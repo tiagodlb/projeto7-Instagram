c9201d4212a8d6a2f7dbabb46875555e4190b8a9
Tiago de Lima Batista <tigolimab223@gmail.com>
2022-03-13 03:38:41 -0300

    Componentização do projeto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./Componentes/Navbar";
+import Corpo from "./Componentes/Conteudo";
+import Footer from "./Componentes/Footer";
+
+export default function App(){
+    return(
+        <div>
+            <Navbar />
+            <Corpo />
+            <Footer />
+        </div>
+    )
+}
